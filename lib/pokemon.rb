5396fa7d6f6a819cc1e66c3ab2ceca704c15c2d8
Michael Carroll <carrollm2@gmail.com>
2020-01-12 18:57:47 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -38,8 +38,7 @@ class Pokemon
     sql = "SELECT * FROM pokemon WHERE id = ?"
     result = db.execute(sql, id)[0]
 
-    Pokemon.create(result[0], result[1], result[2])
-    # Pokemon.new(result[0], result[1], result[2])
+    Pokemon.new(result[0], result[1], result[2])
   end
 
 end
